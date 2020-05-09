9dcfe8f430f3b7e89110203a6263cc107329a466
ckr344 <ckr344.225@gmail.com>
2020-05-09 13:47:37 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 import React from 'react';
 
-function App() {
-  // const greeting = "Hi!!";
-  // const dom = <h1 className="foo">{greeting}</h1>
-  // return (
-  //   dom
-  // );
-
-  // return (
-  //   <input type="text" onClick={() => {console.log("I am clicked.")}} />
-  // );
-
-  // return (
-  //   <input type="text" onChange={() => {console.log("I am clicked.")}} />
-  // );
-
-  // return (
-  //   <div>
   //     <label htmlFor="bar"> bar </label>
-  //     <input type="text" onChange={() => {console.log("I am clicked.")}} />
-  //   </div>
-  // );
+// function App() {
+//   // const greeting = "Hi!!";
+//   // const dom = <h1 className="foo">{greeting}</h1>
+//   // return (
+//   //   dom
+//   // );
 
-  return (
-    <React.Fragment>
-      <label htmlFor="bar"> bar </label>
-      <input type="text" onChange={() => {console.log("I am clicked.")}} />
-    </React.Fragment>
-  );
+//   // return (
+//   //   <input type="text" onClick={() => {console.log("I am clicked.")}} />
+//   // );
+
+//   // return (
+//   //   <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//   // );
+
+//   // return (
+//   //   <div>
+//   //     <label htmlFor="bar"> bar </label>
+//   //     <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//   //   </div>
+//   // );
+
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar"> bar </label>
+//       <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
+  return(
+    <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+  )
 }
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
+
+// class App extends Component {
+//   render(){
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar"> bar </label>
+//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
 
 export default App;
